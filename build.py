bfdb3cf99ddeca0b657acb44657472741b619130
Liam Swayne <liamtswayne@gmail.com>
2023-09-14 16:11:04 -0400

    test

--- build.py
+++ build.py
@@ -14,20 +14,20 @@ css_style = """
     font-family: 'Helvetica Neue', sans-serif;
     text-align: center;
   }
-  .block {
+  .markdown-body {
     max-width: 500px;
     margin: 0 auto 20px;
     text-align: left;
     padding: 10px;
     border: 1px solid white;
   }
-  .title {
+  .markdown-title {
     font-weight: bold;
   }
-  .image {
+  .markdown-image {
     max-width: 100%;
   }
-  .caption {
+  .markdown-caption {
     color: grey;
   }
 </style>
@@ -47,30 +47,34 @@ in_block = False
 for line in markdown_content:
     line = line.strip()
     if line.startswith("# "):  # Title
-        html_output += "<div class='block title'>"
+        html_output += "<div class='markdown-body markdown-title'>"
         html_output += line[2:]  # Remove the "#" symbol
         html_output += "</div>"
     elif line.startswith("![]("):  # Image
         # Extract the image URL
         image_url = line[line.find("(") + 1:line.find(")")]
-        html_output += "<div class='block'>"
-        html_output += "<img class='image' src='" + image_url + "' />"
-        html_output += "<div class='caption'>[caption] This is a dog.</div>"
+        html_output += "<div class='markdown-body'>"
+        html_output += "<img class='markdown-image' src='" + image_url + "' />"
+        html_output += "<div class='markdown-caption'>[caption] This is a dog.</div>"
         html_output += "</div>"
     elif line.startswith("[caption] "):  # Caption
-        html_output += "<div class='caption'>"
-        html_output += line[len("[caption] "):]
-        html_output += "</div>"
+        if in_block:
+            html_output += "<div class='markdown-caption'>"
+            html_output += line[len("[caption] "):]
+            html_output += "</div>"
+        else:
+            html_output += "<div class='markdown-body markdown-caption'>"
+            html_output += line[len("[caption] "):]
+            html_output += "</div>"
     elif line:  # Text block
         if not in_block:
-            html_output += "<div class='block'>"
+            html_output += "<div class='markdown-body'>"
             in_block = True
         html_output += line
     else:  # Empty line
         if in_block:
             html_output += "</div>"
             in_block = False
-        html_output += "<div class='block'></div>"
 
 # Close the last text block if necessary
 if in_block:
--- source.md
+++ source.md
@@ -1,6 +1,7 @@
 # This is markdown
 
-This is a markdown text block. It is still going. Lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet
+This is a markdown text block. It is still going. Lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet lorem ipsum dolor sit ahmet.
+This is part of the same paragraph.
 
 ![](https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*)
 [caption] This is a dog.
